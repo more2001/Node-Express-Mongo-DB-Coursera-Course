4673e8dcaff6b681f451abb5d58e8011e6ae55d6
Shubham <shubhamboss1251@gmail.com>
2023-02-22 01:52:00 +0530

    passport and jwt token

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const Dishes = require("./models/dishes");
-<<<<<<< Updated upstream
 
-=======
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
->>>>>>> Stashed changes
 
 
 const url = config.mongoUrl;
@@ -46,70 +43,11 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
 
-<<<<<<< Updated upstream
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  cookie: { maxAge: 172800000 },
-  store: new FileStore()
-}));
-=======
 app.use(passport.initialize());
->>>>>>> Stashed changes
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-<<<<<<< Updated upstream
-function auth (req, res, next) {
-  console.log(req.session);
-
-  if(!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
-  }
-}
-/*
-
-1. authHeader.split(' ') - This splits the authentication header into an array with two elements: "Basic" and the encoded credentials.
-
-Example: ['Basic', 'dXNlcjpwYXNzd29yZA==']
-
-2. authHeader.split(' ')[1] - This retrieves the second element from the array, which contains the encoded credentials.
-
-Example: 'dXNlcjpwYXNzd29yZA=='
-
-3. new Buffer.from(authHeader.split(' ')[1], 'base64') - This creates a new Buffer object from the base64-encoded string.
-
-Example: <Buffer 75 73 65 72 3a 70 61 73 73 77 6f 72 64>
-
-4. .toString() - This converts the Buffer object to a string.
-
-Example: 'user:password'
-
-5.  .split(':') - This splits the string into an array with two elements: the username and password.
-
-Example: ['user', 'password']
-
-*/
-
-
-app.use(auth);
-=======
->>>>>>> Stashed changes
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,8 @@ var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 var User = require("../models/user");
-<<<<<<< Updated upstream
-=======
 var passport = require('passport');
 var authenticate = require('../authenticate');
->>>>>>> Stashed changes
 
 router.use(bodyParser.json());
 
@@ -26,15 +23,6 @@ router.post('/signup', (req, res, next) => {
     else {
       return User.create({username: req.body.username, password: req.body.password});
     }
-<<<<<<< Updated upstream
-  })
-  .then((user) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({status: 'Registration Successful!', user: user});
-  }, (err) => next(err))
-  .catch((err) => next(err));
-=======
   });
 });
 
@@ -43,7 +31,6 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
->>>>>>> Stashed changes
 });
 
 router.post('/login', (req, res, next) => {
