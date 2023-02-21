a8ae616c24eb2e9229c9d9e5b552f2601dc87b98
Shubham <shubhamboss1251@gmail.com>
2023-02-21 15:10:08 +0530

    "passport and sessions"

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const Dishes = require("./models/dishes");
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 
 const url = "mongodb://localhost:27017/conFusion";
@@ -49,26 +50,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
+  if(!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 /*
