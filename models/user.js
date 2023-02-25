ed092f27fee9d2a56f8653b873387e7049a0761c
Shubham More <shubhamboss1251@gmail.com>
2023-02-25 20:06:01 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,23 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-// var User = new Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password:  {
-//         type: String,
-//         required: true
-//     },
-//     admin:   {
-//         type: Boolean,
-//         default: false
-//     }
-// });
-
-
 var User = new Schema({
     admin:   {
         type: Boolean,
