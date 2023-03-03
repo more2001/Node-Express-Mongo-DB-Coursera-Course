abc37f4bc47b95eb06c77e057b61c31acc9718ec
Shubham <shubhamboss1251@gmail.com>
2023-03-03 23:54:35 +0530

    OAuth and Facebook login

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '898452987944938',
+        clientSecret: 'd9f05b20ebd61835cd5e2a368a923048'
+    }
 }
